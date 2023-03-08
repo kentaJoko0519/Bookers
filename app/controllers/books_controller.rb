e71f23ab6aec4a92b8dd11d2fa6a6f8e5fae8c39
KentaJoko <126621899+kentaJoko0519@users.noreply.github.com>
2023-03-08 13:52:11 +0000

    [削除機能追加]Books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,10 @@
 class BooksController < ApplicationController
-  
+
   def create
     book=Book.new(book_params)
     book.save
-    redirect_to '/top'
-    
+    redirect_to book_path(book.id), method: :get
+
   end
 
   def index
@@ -19,16 +19,22 @@ class BooksController < ApplicationController
   def edit
     @book=Book.find(params[:id])
   end
-  
+
   def update
     book=Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
   end
-  
+
+  def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
