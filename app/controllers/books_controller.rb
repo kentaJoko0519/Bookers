68ed9ea63c24128511e5c47e78e9267ebe98b3fb
KentaJoko <126621899+kentaJoko0519@users.noreply.github.com>
2023-03-08 05:59:50 +0000

    [詳細機能追加レイアウト未]Books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,4 @@
 class BooksController < ApplicationController
-  def new
-    @book=Book.new
-  end
   
   def create
     book=Book.new(book_params)
@@ -11,10 +8,12 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book=Book.new
     @books=Book.all
   end
 
   def show
+    @book=Book.find(params[:id])
   end
 
   def edit
